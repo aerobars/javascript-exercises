480653f51f6ba5a59a09eb3ae136fa64ecc84033
Adrian <adrianbyers@gmail.com>
2023-03-27 21:29:14 -0400

    Update .js and .spec.js files

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+//go through the string a character at a time, starting from the end, and add the letters to a new block variable
+let tempString = '';
+for (let i = inputString.length - 1; i >= 0; i--){
+  tempString += inputString.charAt(i);
+}
+return tempString;
 };
 
 // Do not edit below this line
