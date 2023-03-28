4dcae02ffde6ebd65f21160e69583b4b5eb7b433
Adrian <adrianbyers@gmail.com>
2023-03-27 21:34:49 -0400

    Add psuedocode

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
 const removeFromArray = function() {
+//remove any inputted values found in input array
+//create block scope variable
+//for loop to go through array values
+//if any array values match inputed values, remove it from array
+//return updated array or block scope variable?
 
 };
 
