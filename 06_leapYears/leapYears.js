5d6e828fea1da97c9924870ba6a76dde22f1ce07
Adrian <adrianbyers@gmail.com>
2023-03-29 11:54:35 -0400

    Update js and spec files

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  //check if year is divisible by 4
+  //check if year is divisible by 100 and not divisible by 400, if true return false
+  //return true if both checks are passed, else return false
+  if (year % 4 == 0) {
+    if (year % 100 == 0 && year % 400 != 0) {
+      return false;
+    }
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
