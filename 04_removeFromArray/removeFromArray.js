d1e7c9b9e0c8df550ee182efadc970e80155bdba
Adrian <adrianbyers@gmail.com>
2023-03-28 20:49:03 -0400

    Update js and spec files

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,10 +1,20 @@
 const removeFromArray = function() {
+
 //remove any inputted values found in input array
 //create block scope variable
 //for loop to go through array values
-//if any array values match inputed values, remove it from array
+//nested for loop to go through function arguments
+//if any array values match argument values, remove it from array
 //return updated array or block scope variable?
-
+  const inputArray = arguments[0];
+  for (let i = 1; i < arguments.length; i++) {
+    for (let j = 0; j < inputArray.length; j++){
+      if (inputArray[j] === arguments[i]) {
+        inputArray.splice(j, 1)
+      }
+    }
+  }
+  return inputArray;
 };
 
 // Do not edit below this line
