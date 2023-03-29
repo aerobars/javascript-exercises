38b78b370dff33d68ac2e5cd223bf1fa003a6f5f
Adrian <adrianbyers@gmail.com>
2023-03-29 12:21:14 -0400

    Update js and spec files

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(F) {
+  //use Math.toRound(number * 10) / 10 to get one decimal
+  //(x − 32) × 5/9 °C
+  return Math.round(((F - 32) * 5/9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(C) {
+  //(x × 9/5 + 32) °F
+  return Math.round((C * 9/5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
